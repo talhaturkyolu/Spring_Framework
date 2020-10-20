ebf6fcf0564f1b03a0c7685eab2f933e75f00aa5
Talha Turkyolu <turkyolutalha@gmail.com>
2020-10-20 18:23:15 -0500

    DI-Constructor-Injection

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/School/SchoolApp.java
@@ -0,0 +1,17 @@
+package School;
+
+import School.Interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SchoolApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("Java",Course.class);
+
+        course.getTeachingHours();
+    }
+}
