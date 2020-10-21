434de60899f3ee6793f824f7ce66ce92b875270b
Talha Turkyolu <turkyolutalha@gmail.com>
2020-10-20 19:20:03 -0500

    DI-Setter-Injection

---
+++ Spring-02-DI-SetterInjection/src/main/java/School/services/Java.java
@@ -0,0 +1,24 @@
+package School.services;
+
+import School.interfaces.Course;
+
+public class Java implements Course {
+
+
+
+
+    private  OfficeHours OfficeHours;
+
+    public void setGetOfficeHours(OfficeHours getOfficeHours) {   //set + officeHours
+        this.OfficeHours = getOfficeHours;
+    }
+
+    public OfficeHours getGetOfficeHours() {
+        return OfficeHours;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : " + (20+OfficeHours.getHours()));
+    }
+}
